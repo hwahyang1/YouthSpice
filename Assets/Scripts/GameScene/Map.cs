e88e45509a5942f3c58bac23603af82f5655569b
poppy04 <115389708+poppy04@users.noreply.github.com>
2023-02-15 05:51:52 +0900

    20230215
    완성

--- Assets/Scripts/GameScene/Map.cs
+++ Assets/Scripts/GameScene/Map.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
+using YouthSpice.GameScene;
 using YouthSpice.PreloadScene.Item;
 
 namespace YouthSpice
@@ -11,19 +13,41 @@ namespace YouthSpice
 	/// </summary>
 	public class Map : MonoBehaviour
 	{
-		
-		private void Start()
-		{
-			
+		public Research research;
+		[SerializeField] private GameObject mapCanvas;
+		[SerializeField] private Sprite[] backgroundSprite; //탐색 상태 이미지
+
+		[SerializeField] private Image backgroundImage;
+		public Button mapSelectBtn;
+		public void OceanMap()
+		{ 
+			research.GetComponent<Research>().isOcean = true;
+			research.GetComponent<Research>().isGround = false;
+			research.GetComponent<Research>().isMountain = false;
+			backgroundImage.sprite = backgroundSprite[0];
+			mapCanvas.SetActive(false);
 		}
 
-		private void Update()
+		public void GroundMap()
 		{
-			
+			research.GetComponent<Research>().isOcean = false;
+			research.GetComponent<Research>().isGround = true;
+			research.GetComponent<Research>().isMountain = false;
+			backgroundImage.sprite = backgroundSprite[1];
+			mapCanvas.SetActive(false);
 		}
-		private void RandomIngredient(/*음,,,,,,,,,*/)
+		public void MountainMap()
 		{
-			
-		}	
+			research.GetComponent<Research>().isOcean = false;
+			research.GetComponent<Research>().isGround = false;
+			research.GetComponent<Research>().isMountain = true;
+			backgroundImage.sprite = backgroundSprite[2];
+			mapCanvas.SetActive(false);
+		}
+
+		public void MapSelect()
+		{
+			mapCanvas.SetActive(true);
+		} 
 	}
 }
